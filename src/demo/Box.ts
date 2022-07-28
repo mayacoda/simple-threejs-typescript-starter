c0ab23f0f38692c8a3cd819834f80584af4ee18c
Maya Nedeljković Batić <maya.nedeljko@gmail.com>
2022-07-28 09:28:12 +0200

    Update to vite 3.0.3

--- src/demo/Box.ts
+++ src/demo/Box.ts
@@ -1,9 +1,14 @@
 import * as THREE from 'three'
+import vertexShader from './shader.vert'
+import fragmentShader from './shader.frag'
 
 export class Box extends THREE.Mesh {
   constructor() {
     const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
-    const material = new THREE.MeshStandardMaterial({ color: 0xff00ff })
+    const material = new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+    })
 
     super(geometry, material)
   }
