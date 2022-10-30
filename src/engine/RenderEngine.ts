e723e02d1a0ec3a5b697629e6f7c2b19bba1daff
Maya Nedeljković Batić <maya.nedeljko@gmail.com>
2022-10-30 18:45:03 +0100

    ✨Add effect composer to render engine

--- src/engine/RenderEngine.ts
+++ src/engine/RenderEngine.ts
@@ -2,9 +2,12 @@ import { WebGLRenderer } from 'three'
 import { Engine } from './Engine'
 import * as THREE from 'three'
 import { GameEntity } from './GameEntity'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
 
 export class RenderEngine implements GameEntity {
   private readonly renderer: WebGLRenderer
+  composer: EffectComposer
 
   constructor(private engine: Engine) {
     this.renderer = new WebGLRenderer({
@@ -21,13 +24,23 @@ export class RenderEngine implements GameEntity {
     this.renderer.setClearColor('#000000')
     this.renderer.setSize(this.engine.sizes.width, this.engine.sizes.height)
     this.renderer.setPixelRatio(Math.min(this.engine.sizes.pixelRatio, 2))
+
+    this.composer = new EffectComposer(this.renderer)
+
+    const renderPass = new RenderPass(
+      this.engine.scene,
+      this.engine.camera.instance
+    )
+    this.composer.addPass(renderPass)
   }
 
   update() {
-    this.renderer.render(this.engine.scene, this.engine.camera.instance)
+    this.composer.render()
   }
 
   resize() {
     this.renderer.setSize(this.engine.sizes.width, this.engine.sizes.height)
+    this.composer.setSize(this.engine.sizes.width, this.engine.sizes.height)
+    this.composer.render()
   }
 }
